d4f1c5049cdc8994f49dbbeab71a384192fbcde1
Ahmed Valiaiev <ahmed.valiaiev@instant.co>
2022-04-19 03:11:18 +0200

    init

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import { createUserHandler } from "../controller/user.controller";
+import validateResource from "../middleware/validateResource";
+import { createUserSchema } from "../schema/user.schema";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/",
+  validateResource(createUserSchema),
+  createUserHandler
+);
+
+export default router;
