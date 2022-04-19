60c820093c690ac28171001233c702ed4a51fb46
Ahmed Valiaiev <ahmed.valiaiev@instant.co>
2022-04-19 20:21:20 +0200

    Added auth middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,6 +6,7 @@ import {
   resetPasswordHandler,
   verifyUserHandler,
 } from "../controller/user.controller";
+import requireUser from "../middleware/requireUser";
 import validateResource from "../middleware/validateResource";
 import {
   createUserSchema,
@@ -40,6 +41,6 @@ router.post(
   resetPasswordHandler
 );
 
-router.get('/api/users/me', getCurrentUserHandler)
+router.get("/api/users/me", requireUser, getCurrentUserHandler);
 
 export default router;
